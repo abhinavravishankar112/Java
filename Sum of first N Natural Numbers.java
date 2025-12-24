a54285635f71a2f2a64cc7dbaaa16bb1aa7d4995
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2025-12-24 10:59:16 +0530

    aneq

--- Sum of first N Natural Numbers.java
+++ Sum of first N Natural Numbers.java
@@ -1,12 +1,12 @@
-import java.util.Scanner;
-
-public class SumOfNaturalNumbers {
+public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        int n = 10;
+        int sum = 0;
 
-        int N = sc.nextInt();
-        int sum = N * (N + 1) / 2;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
 
-        System.out.println(sum);
+        System.out.println("Sum = " + sum);
     }
 }
