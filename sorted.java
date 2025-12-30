fb393a6a7913841030a6703f3b3947d91bbd2a34
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2025-12-30 10:59:51 +0530

    30

---
+++ sorted.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CheckArraySorted {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input size
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        // Input array elements
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Check if sorted
+        boolean isSorted = true;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        // Output result
+        System.out.println(isSorted);
+    }
+}
